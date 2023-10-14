58986407b070c6621da20d93f0c39830d777be73
zhixuanQin <1791342660@qq.com>
2023-10-14 17:25:58 +0800

    modified proj1b

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,6 +1,6 @@
 public class OffByOne implements CharacterComparator {
     @Override
-    public boolean equalChars(char x, char y){
+    public boolean equalChars(char x, char y) {
         int dif1 = x - y;
         int dif2 = y - x;
         return dif1 == 1 || dif2 == 1;
--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -2,11 +2,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 public class TestOffByN {
 
-    public static OffByN offByN5 = new OffByN(5);
-    public static OffByN offByN8 = new OffByN(8);
+    static OffByN offByN5 = new OffByN(5);
+    static OffByN offByN8 = new OffByN(8);
 
     @Test
-    public void TestEqualChars() {
+    public void testEqualChars() {
         assertTrue(offByN5.equalChars('a', 'f'));
         assertFalse(offByN8.equalChars('a', 'f'));
     }
