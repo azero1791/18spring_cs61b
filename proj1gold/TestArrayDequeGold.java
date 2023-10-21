77a24718c44a0abf30c27482776b611bcc7ba8bb
zhixuanQin <1791342660@qq.com>
2023-10-21 13:29:42 +0800

    modified proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -9,11 +9,13 @@ public class TestArrayDequeGold {
         ArrayDequeSolution<Integer> aDS = new ArrayDequeSolution<>();
 
         int testTimes = StdRandom.uniform(1000, 5000);
+        Integer testWay = 0;
+        Integer testInteger = 0;
+        Integer expected = 0;
+        Integer actual = 0;
+        String message = "";
         for (int i = 0; i < testTimes; i++) {
-            Integer testWay = 0;
-            Integer testInteger = 0;
-            Integer expected = 0;
-            Integer actual = 0;
+
             if (aDS.isEmpty()) {
                 testWay = StdRandom.uniform(0, 2);
                 testInteger = StdRandom.uniform(-1000, 1000);
@@ -23,16 +25,14 @@ public class TestArrayDequeGold {
                         aDS.addFirst(testInteger);
                         expected = aDS.get(0);
                         actual = sAD.get(0);
-                        assertEquals("addFirst(" + testInteger + ")\nget(0)",
-                                expected, actual);
+                        message += "addFirst(" + testInteger + ")\n";
                         break;
                     case 1 :
                         sAD.addLast(testInteger);
                         aDS.addLast(testInteger);
                         expected = aDS.get(aDS.size() - 1);
                         actual = sAD.get(aDS.size() - 1);
-                        assertEquals("addLast(" + testInteger + ")\nget(size - 1)",
-                                expected, actual);
+                        message += "addLast(" + testInteger + ")\n";
                         break;
                 }
             }   else {
@@ -42,36 +42,26 @@ public class TestArrayDequeGold {
                         testInteger = StdRandom.uniform(-1000, 1000);
                         sAD.addFirst(testInteger);
                         aDS.addFirst(testInteger);
-                        actual = sAD.get(0);
-                        expected = aDS.get(0);
-                        assertEquals("addFirst(" + testInteger + ")\nget(0)",
-                                expected, actual);
+                        message += "addFirst(" + testInteger + ")\n";
                         break;
                     case 1 :
                         sAD.addLast(testInteger);
                         aDS.addLast(testInteger);
-                        expected = aDS.get(aDS.size() - 1);
-                        actual = sAD.get(aDS.size() - 1);
-                        assertEquals("addLast(" + testInteger + ")\nget(size - 1)",
-                                expected, actual);
+                        message += "addLast(" + testInteger + ")\n";
                         break;
                     case 2 :
                         actual = sAD.removeFirst();
                         expected = aDS.removeFirst();
-                        assertEquals("removeFirst()",
-                                expected, actual);
+                        message += "removeFirst()\n";
                         break;
                     case 3 :
                         actual = sAD.removeLast();
                         expected = aDS.removeLast();
-                        assertEquals("removeFirst",
-                                expected, actual);
+                        message += "removeLast()\n";
                         break;
                 }
             }
-
+            assertEquals(message, expected, actual);
         }
-
-
     }
 }
